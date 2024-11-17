718973a2cb5f9d3661f1ec34d60d5660eec808e2
suika05 <dev.suika05@gmail.com>
2024-11-17 22:04:53 +0900

    adjust

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,29 @@
-import { createApp } from 'vue';
+window.addEventListener('load',()=>{
+	todo.init();
+});
 
+const todo = {
+	state: {
+		app:null,
+	},
+	set() {
+		this.state.app = Vue.createApp({
+      methods: {
+				inputText(e) {
+					
+				},
+        template(e) {
+					const _template = document.querySelector('template');
+					const _html = _template.content.cloneNode(true);
+					console.log(_html.querySelector('li'))
+
+					document.querySelector('[data-task]').insertAdjacentHTML('beforeend',_html);
+        },
+      },
+    });
+    this.state.app.mount('#app');
+	},
+	init() {
+		this.set();
+	}
+}
